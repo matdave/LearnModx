0a4825f9309636554beacc64635e6fa957e02497
OptimusCrime <thomasgautv@hotmail.com>
2014-12-25 17:26:58 +0100

    Working on implementing dynamic chapters and sections

--- core/components/learnmodx/model/learnmodx/learnmodx.class.php
+++ core/components/learnmodx/model/learnmodx/learnmodx.class.php
@@ -24,8 +24,12 @@ class LearnModx {
     /**
      * Variables
      */
-    
+
     private $modx;
+    private $elementsPath;
+    private $chapter;
+    private $section;
+    private $chapters;
     
     /**
      * Constructs the LearnModx object
@@ -36,8 +40,16 @@ class LearnModx {
     function __construct(modX &$modx, array $config = array()) {
         $this->modx = &$modx;
 
+        // Load chapter and section here
+        $this->chapter = 0;
+        $this->section = 0;
+
+        // Usual stuff
         $basePath = $this->modx->getOption('learnmodx.core_path', $config,$this->modx->getOption('core_path') . 'components/learnmodx/');
         $assetsUrl = $this->modx->getOption('learnmodx.assets_url', $config,$this->modx->getOption('assets_url') . 'components/learnmodx/');
+
+        $this->elementsPath = $basePath . 'elements/';
+
         $this->config = array_merge(array(
             'basePath' => $basePath,
             'corePath' => $basePath,
@@ -50,11 +62,17 @@ class LearnModx {
             'assetsUrl' => $assetsUrl,
             'connectorUrl' => $assetsUrl . 'connector.php',
 
-            'chapter' => 1,
-            'section' => 0,
+            // LearnModx stuff
+            'elementsPath' => $this->elementsPath,
+            'chapter' => $this->chapter,
+            'section' => $this->section,
         ), $config);
 
+
         $this->modx->addPackage('learnmodx', $this->config['modelPath']);
+
+        // Load all chapters
+        $this->getAllChapters();
     }
 
     /**
@@ -76,5 +94,96 @@ class LearnModx {
             break;
         }
         return true;
+    }
+
+    /**
+     * Getters for chapters and sections
+     */
+
+    public function getChapter() {
+        return $this->chapter;
+    }
+    public function getSection() {
+        return $this->section;
+    }
+
+    /**
+     * Return all chapters
+     */
+
+    public function getAllChapters() {
+        if ($this->chapters == null) {
+            // Hold all chapters
+            $this->chapters = array();
+
+            // Get all chapters
+            $dirs = glob($this->elementsPath . '*' , GLOB_ONLYDIR);
+
+            // Loop all chapters
+            foreach ($dirs as $dir) {
+                // Get chapter id
+                $dir_split = explode('/', $dir);
+                $num = $dir_split[count($dir_split) - 1];
+
+                // Include class
+                require_once $dir . '/chapter' . $num . '.php';
+
+                // Get variables from class
+                $vars = get_class_vars('Chapter' . $num);
+
+                // Get information about class
+                $this->chapters[] = array(
+                    'id' => $num,
+                    'chapter' => $vars['chapter'] . ' - ' . $vars['name'],
+                );
+            }
+        }
+
+
+        // Return chapters
+        return $this->chapters;
+    }
+
+    /**
+     * Return section for a given chapter
+     */
+
+    public function getSectionsForChapter($chapter) {
+        return call_user_func_array(array('Chapter' . $this->chapter, 'getSections'), array());
+    }
+
+    /**
+     * Returns name for a given chapter
+     */
+
+    public function getChapterById($chapter_id) {
+        $chapters = $this->getAllChapters();
+        foreach ($chapters as $chapter) {
+            if ($chapter['id'] == $chapter_id) {
+                return $chapter['chapter'];
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * Returns name for a given section
+     */
+
+    public function getSectionById($chapter, $section) {
+        //
+    }
+
+    /**
+     * Output for processors
+     */
+
+    public function output($arr) {
+        return json_encode(array(
+            'success' => true,
+            'results' => $arr,
+            'total' => count($arr),
+        ));
     }
 }
--- core/components/learnmodx/model/learnmodx/mysql/modlearnmodx.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/modlearnmodx.class.php');
-class modLearnModx_mysql extends modSmartcache{}
